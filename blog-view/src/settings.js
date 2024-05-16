ca8e9851fa1a10af95e628d37a54cc1901ce328d
0铯痕灬 <1728956274@qq.com>
2024-05-16 19:17:18 +0800

    blog

---
+++ blog-view/src/settings.js
@@ -0,0 +1,21 @@
+module.exports = {
+    /**
+     * @type {string}
+     * @description 首页三张背景图
+     */
+    bg1: `https://cdn.jsdelivr.net/gh/0sehen/images/images/18.png`,
+    bg3: `https://cdn.jsdelivr.net/gh/0sehen/images/images/16.png`,
+    /*bg3: `https: //cdn.jsdelivr.net/gh/0sehen/images/images/${getRandomNumber(1, 19)}.png`,
+     */
+
+
+    /**
+     * @type {string}
+     * @description 首页故障风文字
+     */
+    /*malfunctionText: 'Naccl\'s Blog'*/
+}
+
+/*function getRandomNumber(min, max) {
+return Math.floor(Math.random() * (max - min + 1)) + min;
+}*/
